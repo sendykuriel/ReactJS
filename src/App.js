eb156721650455c7000c2aa183ae867bd3013b49
Uriel Sendyk <sendykuriel@gmail.com>
2020-03-21 12:53:06 -0300

    fists states

--- src/App.js
+++ src/App.js
@@ -5,17 +5,53 @@ import './App.css'; //importo el css
 //funcion App devuelve lo que queremos renderizar --> html... :O NO! es JSX
 //usamos className en vez de class ojo! no es html
 
-
+/* 
 function Helloworld(props){
   return (
-    <div id="hello">{props}</div>
+  <div id="hello">
+        <h3>{props.sub}</h3>
+        {props.mytext}
+      </div>
+    
   )
+} */
+
+class Helloworld extends React.Component{
+  
+  state = {
+    show: true
+
+  }
+
+  toggleShow = () => {
+    this.setState({show: !this.state.show})
+  }
+
+
+  render (){
+    if(this.state.show){
+    return(
+      <div id="hello">
+        <h3>{this.props.sub}</h3>
+        {this.props.mytext}
+        <button onClick={this.toggleShow}> Toggle Show</button>
+      </div>
+      
+    )} else {return <h1>no hay elementos
+                      <button onClick={this.toggleShow} >Toggle Show</button>
+                    </h1>}
+  }
+
 }
 
 //formato función:
 function App() {
   return (
-    <div>This is my componente: <Helloworld mytext="Hello Gente"/><Helloworld/><Helloworld/> </div>
+    <div>
+      This is my componente: <Helloworld mytext="Hello Gente" sub = "lorem"/>
+      <Helloworld mytext="Ke Onda!"/>
+      <Helloworld mytext = "Adios"/>
+    </div>
   );
 }
 
